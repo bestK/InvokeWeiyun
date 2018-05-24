5db4364e749f594d838838d7cf0a1ceeac26f18e
ECH2O <1995kaikai@gmail.com>
2018-05-24 16:45:42 +0800

    :art:

--- src/main/java/com/ech2o/weiyun/controller/Magnet2DownloadController.java
+++ src/main/java/com/ech2o/weiyun/controller/Magnet2DownloadController.java
@@ -1,11 +1,8 @@
 package com.ech2o.weiyun.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.ech2o.weiyun.jsoup.MockWeiyun;
-import org.junit.Test;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,22 +19,24 @@ public class Magnet2DownloadController {
 
     @GetMapping("m2d")
     public String m2d(@RequestParam String magnet) throws IOException {
+        String msg = "错误的磁力链接";
+        JSONObject invokeResult = new JSONObject();
         if (isMagnet(magnet)) {
             MockWeiyun.setCookie("pt2gguin=o2554497177; web_wx_rc=TSIHJV; uin=o2554497177; skey=@dQjxzvoSe; p_uin=o2554497177; pt4_token=NBLBn7mF7ySz6Y5K2t-dBZFlk7nCfq18r5lXi5R8aQA_; p_skey=UxQbYCuaugNTa8XwYSFuK448gjKce-jDEZ5zAZ65sIU_");
-            JSONObject result = JSON.parseObject(MockWeiyun.weiyunOdOfflineDownloadClientSave(MockWeiyun.weiyunOdOfflineDownloadClientBefore(magnet)));
-            String msg = result.getJSONObject("data").getJSONObject("rsp_header").getString("retmsg");
-            if (msg.contains("25313")) {
-                System.out.println(msg);
-                msg = "小号次数已使用完毕";
+            String beforeResult = MockWeiyun.weiyunOdOfflineDownloadClientBefore(magnet);
+            if (beforeResult.contains("3000")) {
+                msg = "试用次数已使用完毕";
+            } else {
+                String saveResult = MockWeiyun.weiyunOdOfflineDownloadClientSave(beforeResult);
+                if (!(saveResult.contains("3000") || saveResult.contains("25313"))) {
+                    invokeResult.put("videoUrl", MockWeiyun.getDownloadUrlByMovieName());
+                    msg = "successfully";
+                }
             }
-            System.out.println(result);
-            result.clear();
-            result.put("msg", msg);
-            result.put("videoUrl", MockWeiyun.getDownloadUrlByMovieName());
-            return result.toJSONString();
-        }
-        return "请输入正确的磁力链接 →_→";
 
+        }
+        invokeResult.put("msg", msg);
+        return invokeResult.toJSONString();
     }
 
     private boolean isMagnet(String magnet) {
